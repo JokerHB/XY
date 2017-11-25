b9fd8285a7c78ca7d951a73a8a246530e3961d71
靳洪博 <aifjhb0815@gmail.com>
2017-11-25 13:00:11 +0800

    -fix bugx

--- Run.py
+++ Run.py
@@ -8,10 +8,10 @@ if __name__ == '__main__':
     import Tool.AutoGenerate
 
     filepath = '/Users/joker/Documents/Code/GitHub/XY/Exp/test.txt'
-    Tool.AutoGenerate.AutoGenerate.Generate(nodeSize=10, edgeSize=45, filepath=filepath)
+    Tool.AutoGenerate.AutoGenerate.Generate(nodeSize=10, edgeSize=1, filepath=filepath)
     print 'generate finish'
     graph = Tool.AutoGenerate.AutoGenerate.Import(filepath)
     print graph.GetModularity()
-    # aco = ACO.ACO.ACO(antNumber=10, iterNumber=10, graph=graph, alpha=1, beta=1, roh=0.8, H=10)
-    # print 'begin run'
-    # aco.RunACO()
+    aco = ACO.ACO.ACO(antNumber=10, iterNumber=10, graph=graph, alpha=1, beta=1, roh=0.8, H=10)
+    print 'begin run'
+    aco.RunACO()
